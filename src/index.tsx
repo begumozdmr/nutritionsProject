39aa2d7aa3f9cc4b46ab62bc4c4c0e4a2ce5e78f
begumozdmr <aysebegumozdemir@gmail.com>
2023-12-09 18:31:00 +0300

    fixed npm

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ContextProviders } from 'context/contextProvider';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache()
